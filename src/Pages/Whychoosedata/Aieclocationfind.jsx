2f3f123cca718daeda2b1819ff8175e662b955ff
sagar acharya <105191121+SagarAch1@users.noreply.github.com>
2024-09-22 10:23:47 +0545

    update pokhara location

--- src/Pages/Whychoosedata/Aieclocationfind.jsx
+++ src/Pages/Whychoosedata/Aieclocationfind.jsx
@@ -49,17 +49,17 @@ const OfficeLocator = () => {
       officeData = {
         address:
           "Putalisadak, Kathmandu, ADBL Bank's Building, AL3, Putalisadak-30, Kathmandu, Nepal",
-        phone: "014217400",
+        phone: "977-01-5908881/5908882",
         openingHours: "09:00 AM - 05:30 PM",
         mapUrl:
           "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.3709997553105!2d85.32043637556089!3d27.705829276183444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb195c580e7a5d%3A0xf35b7e6ce96c9d55!2sAsian%20International%20Education%20and%20Migration%20Pvt%20Ltd!5e0!3m2!1sen!2snp!4v1726823511164!5m2!1sen!2snp",
       };
     } else if (office === "Pokhara") {
       officeData = {
-        address: "Central Pokhara, Pokhara, Nepal",
-        phone: "061-123456",
+        address: "Nivagalli, Chiplodhunga-09, Pokhara, Nepal",
+        phone: "+9779801048957",
         openingHours: "09:00 AM - 05:30 PM",
-        mapUrl: "https://www.google.com/maps/embed?...", // Update with actual URL
+        mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3515.455097395174!2d83.98375838117846!3d28.22386341372524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399595667cb215ff%3A0xfe8b4d02bb7d6674!2sNiva%20Galli!5e0!3m2!1sen!2snp!4v1726979628386!5m2!1sen!2snp", // Update with actual URL
       };
     } else if (office === "Chitwan") {
       officeData = {
